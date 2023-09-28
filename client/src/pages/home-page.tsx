9ac9955257dfefa188c0e20dd1d1fb26aa951bb3
Conner Carraher <connercarraher@gmail.com>
2023-09-28 17:37:04 -0400

    Adding exclamation mark

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -6,7 +6,7 @@ export const HomePage = () => {
             <Toolbar />
             <Box sx={{display: 'flex', flexDirection: 'column', flexGrow: 1, p:3, overflow: 'hidden'}}>
                 <Paper sx={{display: 'flex', height: '100%', p:4}}>
-                    <Typography>Hello World</Typography>
+                    <Typography>Hello World!</Typography>
                 </Paper>
             </Box>
         </Box>
