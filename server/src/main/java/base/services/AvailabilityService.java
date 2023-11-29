02b9e5ac9cb92263e3bfead970f7b55584936db2
graciepulliam <104663433+graciepulliam@users.noreply.github.com>
2023-11-29 15:18:17 -0600

    Update AvailabilityService.java

--- server/src/main/java/base/services/AvailabilityService.java
+++ server/src/main/java/base/services/AvailabilityService.java
@@ -12,15 +12,12 @@ import base.models.AvailabilityDto;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-//importing logger for debugging purposes
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 
 @Service
 public class AvailabilityService {
 
-    private static final Logger logger = LoggerFactory.getLogger(AvailabilityService.class);
     private final AvailabilityRepository availabilityRepository;
 
     @Autowired
@@ -43,32 +40,18 @@ public class AvailabilityService {
     @Transactional
     public void processAndSaveAvailabilities(AvailabilityDto availabilityDTO) {
 
-        logger.info("Processing availability for netID: {}", availabilityDTO.getNetID());
-        try {
+      
         String netId = availabilityDTO.getNetID();
         var timeSlots = availabilityDTO.getTimeSlots(); // Map of days to time slots
     
         // Check if netId already exists and delete existing records
-        
         if (checkIfNetIdExists(netId)) {
-            List<Availability> availabilities = availabilityRepository.findAvailabilities(netId);
-            logger.info("all availabilities with netid: {}", netId);
-
-            for (Availability availability : availabilities) {
-                logger.info("Availability - NetID: {}, Day: {}, TimeSlot: {}", 
-                            availability.getNetID(), availability.getDay(), availability.getTimeSlot());
-            }
-
-            logger.info("netID exists in database: {}", availabilityDTO.getNetID());
             deleteAllAvailabilitiesForNetID(netId);
         }
-        else{
-            logger.info("netI does not exist in database: {}", availabilityDTO.getNetID());
-        }
+        
         
     
         // Convert each entry in the Map to Availability entities
-        
         for (Map.Entry<String, List<String>> entry : timeSlots.entrySet()) {
             String day = entry.getKey();
             List<String> slots = entry.getValue();
@@ -78,17 +61,12 @@ public class AvailabilityService {
                 availability.setNetID(netId);
                 availability.setDay(day);
                 availability.setTimeSlot(slot);
-
-                logger.info("Saving availability object: {}", availability.toString());
                 availabilityRepository.save(availability);
-                logger.info("adding following day and timeslot for netid: {}", availability.getNetID());
-                logger.info("day: {}", availability.getDay());
-                logger.info("timeslot: {}", availability.getTimeSlot());
+
+              
             }
         }
-        } catch (Exception e) {
-            logger.error("Error processing availabilities for netID: {}", availabilityDTO.getNetID(), e);
-        }
+       
         
     }
 }
